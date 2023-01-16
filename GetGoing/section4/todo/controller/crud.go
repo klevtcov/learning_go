e80125a2715eb615067d0f22908adfd4d4676ad7
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-01-16 13:00:14 +0300

    sec 4, less 40

---
+++ GetGoing/section4/todo/controller/crud.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/klevtcov/model"
+	"github.com/klevtcov/views"
+)
+
+func create() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			data := views.PostRequest{}
+			json.NewDecoder(r.Body).Decode(&data)
+			fmt.Println(data)
+			if err := model.CreateTodo(data.Name, data.Todo); err != nil {
+				w.Write([]byte("Some error!"))
+				return
+			}
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(data)
+		} else if r.Method == http.MethodGet {
+			data, err := model.ReadAll()
+			if err != nil {
+				w.Write([]byte(err.Error()))
+			}
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(data)
+		}
+	}
+}
--- GetGoing/section4/todo/model/create.go
+++ GetGoing/section4/todo/model/create.go
@@ -2,8 +2,8 @@ package model
 
 import "fmt"
 
-func CreateTodo() error {
-	insertQ, err := con.Query("INSERT INTO TODO VALUES($1, $2)", "Sergey", "This course")
+func CreateTodo(name, todo string) error {
+	insertQ, err := con.Query("INSERT INTO TODO VALUES($1, $2)", name, todo)
 	defer insertQ.Close()
 	if err != nil {
 		fmt.Println(err)
