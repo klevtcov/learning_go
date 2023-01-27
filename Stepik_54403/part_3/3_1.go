2f3c9bf05d55c566ff6d752aceb8d2d366fc00f9
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-01-27 16:27:22 +0300

    up

--- Stepik_54403/part_3/3_1.go
+++ Stepik_54403/part_3/3_1.go
@@ -1,4 +1,4 @@
-package main
+package ma_3_1
 
 import (
 	// "bytes"
---
+++ Stepik_54403/part_3/3_2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(" ")
+}
+
+// Напишите функцию с именем convert, которая конвертирует входное число типа int64 в uint16.
+// Считывать и выводить ничего не нужно!
+// Считайте что функция main и пакет main уже объявлены!
+// func convert(n int64) uint16 {
+// 	return uint16(n)
+// }
+
+// именованный ретёрн
+// func convert(a int64) (b uint16) {
+//     return
+// }
