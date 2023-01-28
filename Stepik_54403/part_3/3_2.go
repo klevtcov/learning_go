b124d9a4f113ec7b9b4f638af38195a46d9c6943
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-01-28 07:04:11 +0300

    3.2(13.14)

--- Stepik_54403/part_3/3_2.go
+++ Stepik_54403/part_3/3_2.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 func main() {
-
-	fmt.Println(" ")
+	fmt.Println(adding("hjj_1kjjd0", "hsadsad3kklasdjns5asd"))
+	// fmt.Println(" ")
 }
 
 // Представьте что вы работаете в большой компании где используется модульная архитектура. Ваш коллега написал модуль с какой-то
@@ -20,6 +25,32 @@ func main() {
 // Ваша функция должна называться adding() !
 // Считайте что функция и пакет main уже объявлены!
 //
+// func adding(str1, str2 string) int64 {
+// 	return clearStringToInteger(str1) + clearStringToInteger(str2)
+// }
+
+// func clearStringToInteger(str string) int64 {
+// 	var result []string
+// 	for _, val := range str {
+// 		if unicode.IsDigit(val) {
+// 			result = append(result, string(val))
+// 		}
+// 	}
+// 	resultStr := strings.Join(result, "")
+// 	resultInt, _ := strconv.Atoi(resultStr)
+// 	return int64(resultInt)
+// }
+//
+// Магия через Трим и вложенные функции
+// func help(r rune) bool {
+//     return !unicode.IsDigit(r)
+// }
+
+// func adding(a, b string) int64 {
+//     n, _ := strconv.ParseInt(strings.TrimFunc(a, help), 10, 64)
+//     m, _ := strconv.ParseInt(strings.TrimFunc(b, help), 10, 64)
+//     return n + m
+// }
 //
 //
 //
