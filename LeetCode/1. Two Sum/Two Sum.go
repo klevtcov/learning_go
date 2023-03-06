2a48d9137a13f0a28c60599b394912340eb35248
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-03-06 20:35:41 +0300

    solved

--- LeetCode/1. Two Sum/Two Sum.go
+++ LeetCode/1. Two Sum/Two Sum.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func twoSum(nums []int, target int) []int {
 	for i, v := range nums {
-		for j, v2 := range nums[i+1:] {
-			fmt.Println("i: ", i, " v: ", v, " j: ", j, " v2: ", v2)
+		for j, v2 := range nums {
+			if i == j {
+				continue
+			}
 			if v+v2 == target {
-				return []int{i, j + 1}
+				return []int{i, j}
 			}
 		}
 	}
@@ -34,3 +36,47 @@ func main() {
 
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
+
+//
+// Можно ускорить проходом с двух сторон
+// func twoSum(nums []int, target int) []int {
+//     if len(nums) <= 1 {
+//         return []int{}
+//     }
+
+//     // Time: O(n)
+//     var ptr1, ptr2 int = 0, len(nums)-1
+//     for ptr1 < ptr2 {
+//         current := nums[ptr2] + nums[ptr1]
+// 		// Time: O(1) best O(3) worst so O(1)
+//         if current == target {
+//             return []int{ptr1, ptr2}
+//         } else if current > target {
+//             ptr2--
+//         } else /* if current < target */ {
+//             ptr1++
+//         }
+//     }
+
+//     return []int{}
+// }
+//
+//
+//
+// Вариант через Хеш-таблицу, но непонятно, в какой момент она создаётся
+// Time: O(n)
+// Space: O(n)
+// func twoSum(nums []int, target int) []int {
+//     // Space: O(n)
+//     s := make(map[int]int)
+
+//     // Time: O(n)
+//     for idx, num := range nums {
+//         // Time: O(1)
+//         if pos, ok := s[target-num]; ok {
+//             return []int{pos, idx}
+//         }
+//         s[num] = idx
+//     }
+//     return []int{}
+// }
