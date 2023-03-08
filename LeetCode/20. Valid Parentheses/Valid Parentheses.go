2ab387f4f789451145da4da1486a24882bae2aab
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-03-08 11:46:36 +0300

    up

--- LeetCode/20. Valid Parentheses/Valid Parentheses.go
+++ LeetCode/20. Valid Parentheses/Valid Parentheses.go
@@ -2,15 +2,51 @@ package main
 
 import "fmt"
 
-func isValid(s string) bool {
+// func isValid(s string) bool {
+// 	dict := map[string]int{
+// 		"(": 0,
+// 		")": 0,
+// 		"[": 0,
+// 		"]": 0,
+// 		"{": 0,
+// 		"}": 0,
+// 	}
+// 	for _, val := range s {
+// 		dict[string(val)] += 1
+// 	}
 
+// 	if dict["("] == dict[")"] && dict["["] == dict["]"] && dict["{"] == dict["}"] {
+// 		return true
+// 	}
+// 	return false
+// }
+
+func isValid(s string) bool {
+	for i := 0; i < len(s); i = i + 2 {
+		switch string(s[i]) {
+		case "(":
+			if string(s[i+1]) != ")" {
+				return false
+			}
+		case "[":
+			if string(s[i+1]) != "]" {
+				return false
+			}
+		case "{":
+			if string(s[i+1]) != "}" {
+				return false
+			}
+		}
+	}
 	return true
 }
 
 func main() {
 	fmt.Println(isValid("()"))
 	fmt.Println(isValid("()[]{}"))
+	fmt.Println(isValid("()[]{}["))
 	fmt.Println(isValid("(]"))
+	fmt.Println(isValid("([)]")) // false
 }
 
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
