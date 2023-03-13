eab2b896d413588d2e658102b580455873b7b52b
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-03-13 14:31:01 +0300

    up

--- RestAPIbyZhashkevych/pkg/service/service.go
+++ RestAPIbyZhashkevych/pkg/service/service.go
@@ -12,6 +12,9 @@ type Autorization interface {
 }
 
 type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
+	GetAll(userId int) ([]todo.TodoList, error)
+	GetById(userId, listId int) (todo.TodoList, error)
 }
 
 type TodoItem interface {
@@ -26,5 +29,6 @@ type Service struct {
 func NewService(repos *repository.Repository) *Service {
 	return &Service{
 		Autorization: NewAuthService(*repos),
+		TodoList:     NewTodoListService(repos.TodoList),
 	}
 }
